80a21e8470c614f6ccae993eca459dfb5b90b890
minchae123 <08minchae17@gmail.com>
2022-11-08 20:43:39 +0900

    1108 ++

--- TetrisAfter/Assets/01_Scripts/SpawnTetromino.cs
+++ TetrisAfter/Assets/01_Scripts/SpawnTetromino.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SpawnTetromino : MonoBehaviour
 {
     public GameObject[] tetroMinoes;
 
+    public static int score;
+    public Text scoreText;
+
+    public Vector3[] spawnPositioins;
+    public GameObject gameOverPanel;
+
+    public Queue<GameObject> nextTetrominoes;
+
+
     private void Start()
     {
+        nextTetrominoes = new Queue<GameObject>();
+        for (int i = 0; i < spawnPositioins.Length; i++)
+        {
+            GenerateNext(i);
+        }
         NewTetroMino();
+    }
+
+
+    private void Update()
+    {
+        if (scoreText) scoreText.text = $"Score : {score}";
+    }
+
+    public void GenerateNext(int i)
+    {
+        GameObject t = Instantiate(tetroMinoes[Random.Range(0, tetroMinoes.Length)], spawnPositioins[i], Quaternion.identity);
+        t.GetComponent<TetrisBlock>().gameOverPanel = gameOverPanel;
+        t.GetComponent<TetrisBlock>().enabled = false;
+        nextTetrominoes.Enqueue(t);
     }
 
     public void NewTetroMino()
     {
-        Instantiate(tetroMinoes[Random.Range(0, tetroMinoes.Length)], transform.position, Quaternion.identity);
+        //Instantiate(tetroMinoes[Random.Range(0, tetroMinoes.Length)], transform.position, Quaternion.identity);
+        
+        GameObject t = nextTetrominoes.Dequeue();
+        t.transform.position = transform.position;
+        t.GetComponent<TetrisBlock>().gameOverPanel = gameOverPanel;
+        t.GetComponent<TetrisBlock>().enabled = true;
+
+        GenerateNext(spawnPositioins.Length - 1);
+        MoveSpawnerPos();
+    }
+
+    public void MoveSpawnerPos()
+    {
+        for (int i = 0; i < spawnPositioins.Length; i++)
+        {
+            GameObject t = nextTetrominoes.Dequeue();
+            t.transform.position = spawnPositioins[i];
+            nextTetrominoes.Enqueue(t);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- TetrisAfter/Assets/01_Scripts/TetrisBlock.cs
+++ TetrisAfter/Assets/01_Scripts/TetrisBlock.cs
@@ -10,8 +10,10 @@ public class TetrisBlock : MonoBehaviour
     public static int width = 10;
     public static int height = 20;
     public Vector3 rotationPoint;
+    public GameObject gameOverPanel;
 
     private static Transform[,] grid = new Transform [width, height];
+
 
     private void Update()
     {
@@ -47,8 +49,14 @@ public class TetrisBlock : MonoBehaviour
             if (!VaildMove())
             {
                 transform.position -= new Vector3(0, -1, 0);
+                //if(fallTime <= 0.7f)
+                if((Time.time - previousTime) >= 0.7f)
+                {
+                    GameOver();
+                    return;
+                }
                 AddToGrid();
-
+                CheckForLines();
                 this.enabled = false;
                 FindObjectOfType<SpawnTetromino>().NewTetroMino();
 
@@ -86,5 +94,61 @@ public class TetrisBlock : MonoBehaviour
 
             grid[roundX, roundY] = child;
         }
+    }
+
+    public void CheckForLines()
+    {
+        for(int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLines(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if (grid[j,i] == null)
+            {
+                return  false;
+            }
+        }
+        return true;
+    }
+
+    void DeleteLines(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+            SpawnTetromino.score++;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
